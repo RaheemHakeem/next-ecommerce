64210117de490712a8eb7796f0800a681d6f29bf
Rav <raheemhakeem25@gmail.com>
2024-01-27 00:49:07 +0100

    Added Account page

---
+++ app/(auth)/account/page.jsx
@@ -0,0 +1,115 @@
+import { styles } from "../../styles";
+
+const Account = () => {
+  return (
+    <div className={`${styles.marginX} mt-20 mb-[180px]`}>
+      <div className="mb-[80px] flex justify-between">
+        <div className="flex gap-2 text-sm ">
+          <p className=" text-slate-500">Home</p>
+          <span className=" text-slate-500">/</span>
+          <p>My Account</p>
+        </div>
+
+        <div className="text-sm">
+          <p className=" font-medium">
+            Welcome! <span className="text-[#db4444]">Sin0fpride</span>
+          </p>
+        </div>
+      </div>
+
+      <div className="flex gap-x-[100px]">
+        <div className=" min-w-fit">
+          <div className="mb-[24px]">
+            <h1 className="text-base font-bold mb-[22px]">Manage My Account</h1>
+            <div className="ml-[35px] text-gray-400 space-y-2">
+              <p>My Profile</p>
+              <p>Address Book</p>
+              <p>My Payment Options</p>
+            </div>
+          </div>
+
+          <div className="mb-[24px]">
+            <h1 className="text-base font-bold mb-[22px]">Manage Orders</h1>
+            <div className="ml-[35px] text-gray-400 space-y-2">
+              <p>My Returns</p>
+              <p>My Cancellations</p>
+            </div>
+          </div>
+
+          <h1 className="text-base font-bold mb-[22px]">My WishList</h1>
+        </div>
+
+        <div className="py-10 px-20">
+          <h1 className=" text-xl font-semibold text-[#db4444] mb-4">
+            Edit Your Profile
+          </h1>
+          <form>
+            <div className="flex gap-x-[50px] text-base mb-4">
+              <div>
+                <h3 className="mb-2">First Name</h3>
+                <input
+                  type="text"
+                  placeholder="Rav"
+                  className="h-[50px] bg-slate-100 w-[330px] p-3 rounded"
+                />
+              </div>
+              <div>
+                <h3 className="mb-2">Last Name</h3>
+                <input
+                  type="text"
+                  placeholder="Keen"
+                  className="h-[50px] bg-slate-100 w-[330px] p-3 rounded"
+                />
+              </div>
+            </div>
+
+            <div className="flex gap-x-[50px] text-base mb-4">
+              <div>
+                <h3 className="mb-2">Email</h3>
+                <input
+                  type="text"
+                  placeholder="ravkeen@gmail.com"
+                  className="h-[50px] bg-slate-100 w-[330px] p-3 rounded"
+                />
+              </div>
+              <div>
+                <h3 className="mb-2">Address</h3>
+                <input
+                  type="text"
+                  placeholder="133, Abeokuta Street,Ebute-Meta. lagos"
+                  className="h-[50px] bg-slate-100 w-[330px] p-3 rounded"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col mb-4">
+              <h3 className="mb-2">Password Changes</h3>
+              <input
+                type="password"
+                placeholder="Current Password"
+                className="h-[50px] bg-slate-100 w-full mb-2 p-3 rounded"
+              />
+              <input
+                type="password"
+                placeholder="New Password"
+                className="h-[50px] bg-slate-100 w-full mb-2 p-3 rounded"
+              />
+              <input
+                type="password"
+                placeholder="Confirm New Password"
+                className="h-[50px] bg-slate-100 w-full p-3 rounded"
+              />
+            </div>
+
+            <div className="flex justify-end">
+              <button>Cancel</button>
+              <button className="w-[215px] h-[56px] bg-[#db4444] rounded text-white text-base ml-8">Save Changes</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Account;
